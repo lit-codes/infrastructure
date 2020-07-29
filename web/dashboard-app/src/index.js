db646e232383da7cfadf185684390188659d7d55
Amin <amin@lit.codes>
2020-07-29 20:20:09 +0800

    Revert web changes

--- web/dashboard-app/src/index.js
+++ web/dashboard-app/src/index.js
@@ -1,19 +1,53 @@
-import "fontsource-roboto/latin-ext.css";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Teacher from './pages/Teacher.js';
+import cubejs from '@cubejs-client/core';
+import { loadTeacher, loadTeacherRatings } from './cubes';
+import { drawUI } from './ui';
 
-function App() {
-  return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Teacher />
-          </Route>
-        </Switch>
-      </Router>
-  );
+// Replaces 3000 to 4000 as a hack for development servers
+const API_URL = document.location.origin.replace('3000', '4000');
+let authPromise;
+
+async function authenticate(url) {
+  const response = await fetch(url);
+  if (response.ok) {
+    console.log('Authenticated!');
+  } else {
+    throw `Authentication failed with error: ${response.statusText}`;
+  }
+  const json = await response.json();
+  return json.token;
 }
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+// Initialize cubejs instance with API Token and API URL
+const api = cubejs(async () => {
+  // Only authenticate once
+  // TODO: May need JWT token renewal
+  if (authPromise) return authPromise;
+
+  authPromise = authenticate(`${API_URL}/auth/cubejs-token`);
+  return authPromise;
+}, {
+  apiUrl: `${API_URL}/cubejs-api/v1`,
+});
+
+function getIdFromSearchString() {
+  const searchString = document.location.search;
+  const match = searchString.match(/\?tid=(\d+)/);
+  if (!match) {
+    // Default to Andrew Ng
+    document.location.search = 'tid=545784';
+    return undefined;
+  }
+  return match[1];
+}
+
+(async function main() {
+  const teacherId = getIdFromSearchString();
+  // Load teacher and ratings in parallel
+  const [teacher, teacherRatings] = await Promise.all([
+    loadTeacher(api, teacherId), loadTeacherRatings(api, teacherId),
+  ]);
+  drawUI({
+    teacher,
+    teacherRatings,
+  });
+}()).then(console.log, console.error);
