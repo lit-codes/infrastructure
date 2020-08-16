ae73c888eb745bba602b6630d7ebb33dba4bb9ba
Amin <amin@lit.codes>
2020-08-16 21:09:23 +0800

    Add create database

--- database/schema.sql
+++ database/schema.sql
@@ -17,6 +17,28 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: rmp; Type: DATABASE; Schema: -; Owner: rmp
+--
+
+CREATE DATABASE rmp WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.utf8' LC_CTYPE = 'en_US.utf8';
+
+
+ALTER DATABASE rmp OWNER TO rmp;
+
+\connect rmp
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: department_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
