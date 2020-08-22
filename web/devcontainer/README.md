d383749f7382cc03636444055f8ae1e648cbefe9
Saeed Kazemi <kazemi.ms@gmail.com>
2020-08-22 23:33:27 +0430

    Added devcontainer

---
+++ web/devcontainer/README.md
@@ -0,0 +1,11 @@
+# RMP & PostgreSQL with Docker-Compose 
+
+Prerequisites
+-------------
+1. install docker
+2. install docker-compose
+
+Getting Started
+---------------
+1. start service: `docker-compose up`
+2. visit [localhost:4000](localhost:4000)
---
+++ web/devcontainer/initdb/2-data.sql
@@ -0,0 +1,24 @@
+COPY school
+FROM '/docker-entrypoint-initdb.d/school.csv'
+DELIMITER ',' 
+CSV HEADER;
+
+COPY department
+FROM '/docker-entrypoint-initdb.d/department.csv'
+DELIMITER ',' 
+CSV HEADER;
+
+COPY teacher
+FROM '/docker-entrypoint-initdb.d/teacher.csv'
+DELIMITER ',' 
+CSV HEADER;
+
+COPY school_ratings
+FROM '/docker-entrypoint-initdb.d/school_ratings.csv'
+DELIMITER ',' 
+CSV HEADER;
+
+COPY teacher_ratings
+FROM '/docker-entrypoint-initdb.d/teacher_ratings.csv'
+DELIMITER ',' 
+CSV HEADER;
