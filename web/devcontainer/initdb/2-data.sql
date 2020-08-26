606ea6b8c55eca9086121f1c0e4f46b9ae6ad6e1
Amin <amin@lit.codes>
2020-08-26 23:14:18 +0800

    Push WIP

--- web/devcontainer/initdb/2-data.sql
+++ web/devcontainer/initdb/2-data.sql
@@ -1,24 +1,24 @@
 COPY school
-FROM '/docker-entrypoint-initdb.d/school.csv'
+FROM '/docker-entrypoint-initdb.d/data/school.csv'
 DELIMITER ',' 
 CSV HEADER;
 
 COPY department
-FROM '/docker-entrypoint-initdb.d/department.csv'
+FROM '/docker-entrypoint-initdb.d/data/department.csv'
 DELIMITER ',' 
 CSV HEADER;
 
 COPY teacher
-FROM '/docker-entrypoint-initdb.d/teacher.csv'
+FROM '/docker-entrypoint-initdb.d/data/teacher.csv'
 DELIMITER ',' 
 CSV HEADER;
 
 COPY school_ratings
-FROM '/docker-entrypoint-initdb.d/school_ratings.csv'
+FROM '/docker-entrypoint-initdb.d/data/school_ratings.csv'
 DELIMITER ',' 
 CSV HEADER;
 
 COPY teacher_ratings
-FROM '/docker-entrypoint-initdb.d/teacher_ratings.csv'
+FROM '/docker-entrypoint-initdb.d/data/teacher_ratings.csv'
 DELIMITER ',' 
 CSV HEADER;
