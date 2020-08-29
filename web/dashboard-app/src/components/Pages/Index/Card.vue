33565a9baa2f68859ef86c62b25fee8c0589e884
peymannaderi3 <peymannaderi3@yahoo.com>
2020-08-29 19:30:45 +0430

    new dashboard added.

---
+++ web/dashboard-app/src/components/Pages/Index/Card.vue
@@ -0,0 +1,46 @@
+<template>
+ <q-card class="my-card">
+      <q-card-section class="bg-light text-dark">
+          <div class="row justify-between" style="min-width: 450px;">
+                <div>
+                    Andrew
+                <q-icon  class="q-ml-sm" :name="fasCopy" />
+                </div>
+                <div class="q-mx-md">
+                    Ratings over time
+                </div>
+                <div>
+                <q-icon  class="q-mr-sm" :name="farTrashAlt" />
+                <q-icon  class="q-mr-sm" :name="fasCopy" />
+                <q-icon  class="q-mr-sm" :name="farTrashAlt" />
+            </div>
+          </div>
+      </q-card-section>
+
+      <q-separator />
+      <div class="q-pa-md">
+        <slot />
+      </div>
+    </q-card>
+</template>
+
+<script>
+import { farTrashAlt, fasCopy } from '@quasar/extras/fontawesome-v5'
+
+export default {
+  data () {
+    return {
+      farTrashAlt: null,
+      fasCopy: null
+    }
+  },
+  created () {
+    this.farTrashAlt = farTrashAlt
+    this.fasCopy = fasCopy
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ web/dashboard-app/src/pages/Index.vue
@@ -0,0 +1,45 @@
+<template>
+  <q-page class="flex flex-start justify-start items-start content-start q-pa-md">
+
+    <Card class="q-ma-xs" >
+      <Bar />
+    </Card>
+
+    <Card class="q-ma-xs">
+      salam
+    </Card>
+
+    <Card class="q-ma-xs">
+      <Score/>
+    </Card>
+
+  </q-page>
+</template>
+
+<script>
+import Card from '../components/Pages/Index/Card'
+import Score from '../components/Pages/Index/Score'
+import Bar from '../components/Common/Chartjs/Bar/Bar'
+import cubejs from '@cubejs-client/core'
+
+// Hack for development, change UI address to API
+const API_URL = document.location.origin
+  .replace('ui', 'api') // Theia dev servers
+  .replace('3000', '4000') // Local development
+
+export default {
+  name: 'PageIndex',
+  components: {
+    Card,
+    Score,
+    Bar
+  },
+  data () {
+    return {
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
