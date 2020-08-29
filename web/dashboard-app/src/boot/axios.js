a925d488ff2436caf0fbb08a3a231f26a4c4905c
peymannaderi3 <peymannaderi3@yahoo.com>
2020-08-29 21:15:22 +0430

    add: cube js authentication.

--- web/dashboard-app/src/boot/axios.js
+++ web/dashboard-app/src/boot/axios.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
 import axios from 'axios'
 
-Vue.prototype.$axios = axios
+const API_URL = document.location.origin
+  .replace('ui', 'api') // Theia dev servers
+  .replace('3000', '4000') // Local development
+
+const axiosInstance = axios.create({
+  baseURL: API_URL
+})
+
+Vue.prototype.$axios = axiosInstance
--- web/dashboard-app/src/pages/Index.vue
+++ web/dashboard-app/src/pages/Index.vue
@@ -1,18 +1,14 @@
 <template>
   <q-page class="flex flex-start justify-start items-start content-start q-pa-md">
-
-    <Card class="q-ma-xs" >
+    <Card class="q-ma-xs">
       <Bar />
     </Card>
 
-    <Card class="q-ma-xs">
-      salam
-    </Card>
+    <Card class="q-ma-xs">salam</Card>
 
     <Card class="q-ma-xs">
-      <Score/>
+      <Score />
     </Card>
-
   </q-page>
 </template>
 
@@ -22,11 +18,6 @@ import Score from '../components/Pages/Index/Score'
 import Bar from '../components/Common/Chartjs/Bar/Bar'
 // import cubejs from '@cubejs-client/core'
 
-// Hack for development, change UI address to API
-// const API_URL = document.location.origin
-//   .replace('ui', 'api') // Theia dev servers
-//   .replace('3000', '4000') // Local development
-
 export default {
   name: 'PageIndex',
   components: {
@@ -35,11 +26,21 @@ export default {
     Bar
   },
   data () {
-    return {
-    }
+    return {}
   },
   mounted () {
-
+    this.authenticate()
+  },
+  methods: {
+    async authenticate () {
+      try {
+        const response = await this.$axios.get('/')
+        console.log('Authenticated2!', response)
+        return response
+      } catch (e) {
+        console.log('Authentication failed with error:', e)
+      }
+    }
   }
 }
 </script>
