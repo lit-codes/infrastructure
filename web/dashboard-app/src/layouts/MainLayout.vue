9bb73a84ca3ee992ef41de4ef0a8a0bb908b1e82
peymannaderi3 <peymannaderi3@yahoo.com>
2020-09-13 03:03:33 +0430

    add: header search input.

--- web/dashboard-app/src/layouts/MainLayout.vue
+++ web/dashboard-app/src/layouts/MainLayout.vue
@@ -2,29 +2,35 @@
   <q-layout view="lHh Lpr lFf" class="bg-dark">
     <q-header elevated>
       <q-toolbar>
-        <q-input dark dense standout style="min-width: 300px" v-model="text" label="Ratings of teachers over time" class="q-ml-md">
-          <template v-slot:append>
-            <q-icon v-if="text === ''" name="search" />
-            <q-icon v-else name="clear" class="cursor-pointer" @click="text = ''" />
+        <q-select
+          v-model="model"
+          dark
+          dense
+          standout
+          style="min-width: 300px"
+          label="Ratings of teachers over time"
+          class="q-ml-md"
+          use-input
+          hide-selected
+          input-debounce="500"
+          :options="options"
+          dropdown-icon="search"
+          @filter="filterFn"
+          @filter-abort="abortFilterFn"
+        >
+          <template v-slot:no-option>
+            <q-item>
+              <q-item-section class="text-grey">No results</q-item-section>
+            </q-item>
           </template>
-        </q-input>
+        </q-select>
 
-        <q-toolbar-title>
-          Codelite App
-        </q-toolbar-title>
+        <q-toolbar-title>Codelite App</q-toolbar-title>
 
         <div>Codelite v{{ $q.version }}</div>
 
-        <q-btn
-          class="q-ml-md q-mr-xs"
-          dense
-          icon="share"
-        />
-        <q-btn
-          dense
-          icon="help"
-        />
-
+        <q-btn class="q-ml-md q-mr-xs" dense icon="share" />
+        <q-btn dense icon="help" />
       </q-toolbar>
     </q-header>
 
@@ -35,13 +41,27 @@
 </template>
 
 <script>
-
 export default {
   name: 'MainLayout',
   data () {
     return {
       leftDrawerOpen: false,
-      text: ''
+      text: '',
+
+      model: null,
+      options: []
+    }
+  },
+  methods: {
+    async filterFn (val, update, abort) {
+      const { data } = await this.$axios.get(`http://127.0.0.1:3001/search?q=${val}`)
+      update(() => {
+        this.options = data.map(item => item._source.name)
+      })
+    },
+
+    abortFilterFn () {
+      // console.log('delayed filter aborted')
     }
   }
 }
