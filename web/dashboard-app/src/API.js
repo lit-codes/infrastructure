1f5374720fe619b7e5597a423352eafc0c3981c7
Amin <ihsaram.nima@gmail.com>
2020-12-23 09:31:19 +0000

    Full screen editor

--- web/dashboard-app/src/API.js
+++ web/dashboard-app/src/API.js
@@ -10,10 +10,12 @@ export default class API {
     async query(query) {
         const response = await fetch(`${API_URL}/v1/graphql`, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ query }),
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify(query),
         });
-        const json = await response.json();
-        return json.data;
+        return response.json().catch(() => response.text());
     }
 }
